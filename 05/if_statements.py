d43bb4d2ad82dfb2d010caa90d5db685449fdc7f
Ben <ben.r.dominguez@gmail.com>
2025-10-12 19:51:04 -0700

    completed Ch05 updated working dirs

--- 05/if_statements.py
+++ 05/if_statements.py
@@ -44,5 +44,97 @@ if 5 not in list:
     print('did not find 5')
 
 # boolean expressions are very useful in conditionals
-full_cart
-cart = ['apple', 'orange', 'mango', 'grape']
+# note the bool value requires capitalization to be used
+identity = 'Niko'
+
+if identity == 'Niko':
+    print('Hello Niko')
+identity = 'Roman'
+if identity != 'Niko':
+    print("You're not Niko!")
+
+# for an `if-else` chain in Python use `elif`
+
+num = [1, 50, 100]
+for digit in num:
+    if digit > 50:
+        print(f'{digit} > 50')
+    elif digit == 50:
+        print(f'{digit} == 50')
+    elif digit < 50:
+        print(f'{digit} < 50')
+
+# because only a single variable in the list is <100 the
+# code can be written as follows:
+# for digit in num:
+#    if digit > 50:
+#        print(f'{digit} > 50')
+#    elif digit == 50:
+#        print(f'{digit} == 50')
+#    else:
+#        print(f'{digit} < 50')
+
+# an appropriate usage of the elif chain is to use only when
+# a single test needs to be passed
+
+# notice how toppings checked for past bacon will not be
+# recognized, this is because it finds the first result in
+# the logical order of the program and returns the results
+# for the specific result.
+# a loop would be more appropriate for this scenario.
+sandwich_toppings = ['bacon', 'lettuce', 'tomato']
+
+if 'bacon' in sandwich_toppings:
+    print('adding mayo')
+    sandwich_toppings.append('mayo')
+elif 'lettuce' in sandwich_toppings:
+    print('adding mustard')
+elif 'tomato' in sandwich_toppings:
+    print('adding salt')
+
+# the code below takes an order and checks for tomato. if
+# tomatoes are ordered they will not be appended to the order
+# list.
+order = []
+    
+for topping in sandwich_toppings:
+    if topping == 'tomato':
+        print("Sorry, we're all out of tomatoes.")
+    else:
+        print(f'{topping}? You got it!')
+        order.append(topping)
+print(f'One sandwich with:')
+
+for items in order:
+    print(items)
+print('coming right up!')
+
+# assume the order list remains empty
+order = []
+
+# the code first checks for an order then for any items will
+# print to terminal. If there are no items the code will 
+# print the else statement to the terminal.
+if order:
+    for items in order:
+        print(f'{items}, you got it!')
+    print('\nAnything else?')
+else:
+    print('Just the bun? Are you sure?')
+    
+# Now add an imaginary menu. 
+# Note the usage of a touple
+menu = ('bacon', 'lettuce', 'tomato', 'mushroom')
+order = ['bacon', 'lettuce', 'tomato', 'mustard']
+
+# the code below takes each item in the order list and for each
+# item that is found in the menu list the console prints
+# the item in question. if the item is not found the console
+# will print a message appologizing for not containing such item.
+for item in order:
+    if item in menu:
+        print(f'{item}, gotcha.')
+    else:
+        print(f'sorry, we dont have {item}')
+print('Can I get you anything else?')
+
--- README.md
+++ README.md
@@ -4,3 +4,9 @@ My attempts at learning to use python using Eric Matthes' 'Python Crash Course' 
 # Guide
 
 Contents under source are the original 'cheat sheet' documents provided by https://github.com/ehmatthes/pcc_3e/
+
+# TODO
+
+Read through PEP 8 style guide to learn about recommended styling for legebility python.org/dev/peps/pep-0008
+
+Code review: create a terminal application that utilizes concepts learned in ch 01 - 04
