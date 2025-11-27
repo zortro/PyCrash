9640bfc3518a63e0505c9d22fd8411a64a99ba0e
Ben <ben.r.dominguez@gmail.com>
2025-11-26 22:38:12 -0800

    still here!!!

--- basics/08/functions.py
+++ basics/08/functions.py
@@ -13,3 +13,22 @@ def greet_user(user):
 # in this case 'james' is the arguement being passed
 # through to greet_user()
 greet_user('james')
+
+# personal arguements are based on the order of arguements provided
+# arguements can have default values declared
+def takeMeasurement(measuredItem, heightInFeet, widthInFeet, measurementType='ft.'):
+    print(f'The {measuredItem} is {heightInFeet} {measurementType} tall, by {widthInFeet} {measurementType} wide.')
+
+# default value will take effect because value was not provided
+takeMeasurement(8, 'wall', 12)
+
+# keywords arguements can be used to prevent confusion in declaration
+takeMeasurement(measuredItem='wall', heightInFeet=8, widthInFeet=12, measurementType='Meters')
+
+# returning a value from the function
+def helloWorld(hello, world):
+    result = f'{hello} {world}'
+    return result
+
+sayHello = helloWorld('hello', 'world')
+print(sayHello)
