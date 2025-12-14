7a7d444418b6aa93ef6b1c24ab926e2838fbf945
Ben <ben.r.dominguez@gmail.com>
2025-12-13 21:59:03 -0800

    making comment edits to be more concise

--- basics/02/numbers.py
+++ basics/02/numbers.py
@@ -1,67 +1,46 @@
-# basic operations such as addition (+), subtraction (-), multiplication (*),
-# and division (/) can be performed in python.
-
-# addition
+# add
 print(1+4)
 # subtract
 print(5-1)
 # multiply
 print(2*3)
-fl
-# division (note that python will return float division by default. Floor division can be used by importing using `//`)
+# divide
+# (note that python will return float division by default. Floor division can be used by importing using `//`)
 print(10/2)
+print(10//2)
 
-# to use exponent values the terminal uses two mulitplication symbols (**)
-
-# output 100
+# exponential values
 print(10**2)
 
 # python also supports the use of the order of operations, meaning several
 # operations can be used in one expression.
-
-# output 10
 print((2+3)*2)
 
-# floats are present as per most programming languages
-
-# output 0.2
+# float addition
 print(0.1+0.1)
 
 # arbitrary values will be present as python attempts to represent the
 # as precisely as possible
-
-# output 0.30000000000000004
 print(0.2+0.1)
+# 0.30000000000000004
 
 # mixing floats and integers will by default return a float value similarly to
 # dividing
-
-# output 3.0
 print(1+2.0)
 
-# when writing long numbers, underscores can be used to make digit placement
-# more legible. Python will ignore underscores in any arrangement.
-large_number = 123_456_789
-small_number = 12_3
-
-# output 123456789
-print(large_number)
-
-# output 123
-print(small_number)
+# underscores can be used to make digit placement, they will be ignored by default
+print(123_456_789)
+print(12_3)
 
 # values can be assigned to multiple variables in a single instance, helping
 # shorten code.
 x, y, z = 0, 1, 2
 
-# output
-# 1
-# 2
-# 3
 print(f"{x}\n{y}\n{z}")
 
-# constant variables are not built into Python. It is common among programmers
-# to use all capital leters to indicate a constant variable.
+# Python is a dynamically typed language
+# This means that variables are dictated at runtime rather than at compile time like C++
 
 # indicates the maximum user quantity
-MAX_USERS = 10
+# caps can be used as an indication that a variable is constant
+FAKE_MAX_USERS = 10
--- basics/02/strings.py
+++ basics/02/strings.py
@@ -1,85 +1,53 @@
-# this time use `message` as a variable containing
-# what terminal output
-
-# output "Hello Python, this message is contained in a variable!"
+# create variable and print variable to terminal
 message = "Hello Python, this message is contained in a variable!"
 print(message)
 
-# using the method title() on our name variable will call for the
-# first character in each word to title case, a capitalized letter.
-
-# output "John Doe"
+# method: title() will convert the first letter in all strings to uppercase
 name = "john doe"
 print(name.title())
 
-# output "JOHN DOE"
+# method: upper() will convert all to uppercase
 print(name.upper())
 
-# output "john doe"
-# note because variable is already lower output is expected to
-# remain the same as input
+# method: lower() will convert all to lowercase
 print(name.lower())
 
 first = "johnny"
 last = "doe"
 
-# utilizing an f string will join variables first and last to create a new
-# variable holding full name.
+# f strings are used to stringify variables
 full = f"{first} {last}"
 
-# output "johnny doe"
 print(full)
 
-# using f strings in combination with the title method
-# output "Hello, Johnny Doe. Welcome to Python!"
 print(f"Hello, {first.title()} {last.title()}. Welcome to Python!")
 
-# containing an f string in a variable
+# f sttrings can be pre stored in a variable
 second_message = f"Greetings {first.title()} {last.title()}. I am... {name.upper()}!!"
 
-# output "Greetings Johnny Doe. I am... JOHN DOE!!"
 print(second_message)
 
-#using `\t` to tab text in output
-
-# output "    Python!"
+# \t will tab over the next printed text that follows
 print("\tPython!")
 
-# using `\n` to create a new line in output
-
-# output
-# "
-# Languages:
-# Python
-# Javascript
-# "
+# \n will create a new line for the next text that follows
 print("Languages:\nPython\nJavaScript")
-
-# using the rstrip() method, whitespace at the end of a string can be removed
-# to python whitespace is distinguishable to the program. Meaning 'Python'
-# and 'Python ' are two seperate values.
 
 whitespace = 'Python '
 
-#output "example with whitespace Python !"
+# method rstrip() will strip any white space from a value
 print(f"example with whitespace {whitespace}!")
 
-# output "example without whitespace Python!"
 print(f"example without whitespace {whitespace.rstrip()}!")
 
-# prefixes can be removed from a string by inputting the expected prefix
-# to the removeprefix() method.
+# method removeprefix() will remove any matching variable that is passed through
 example_url = 'https://example_url.null'
 example_person = 'Mr. Doe'
 
-# output "example_url.null"
 print(example_url.removeprefix('https://'))
 print(example_person.removeprefix('Mr. '))
 
-# To avoid syntax error be sure to wrap strings containing apostraphes with
-# quotation marks. Reflecting on this document shows the interchangeability
-# of apostraphes and quotations as wrappers to strings.
+# avoiding common syntax error:
 final = "A test of john's skill."
 
-# "A test of john's skill."
 print(final)
